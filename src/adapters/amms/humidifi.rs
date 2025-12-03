e97fd1bc409a092fc1115292de6a7f2553b9728e
lbkolev <lachezarkolevgg@gmail.com>
2025-12-03 08:01:16 +0200

    unimpl amm methods

--- src/adapters/amms/humidifi.rs
+++ src/adapters/amms/humidifi.rs
@@ -23,4 +23,39 @@ impl Amm for Humidifi {
     fn label(&self) -> String {
         "HumidiFi".to_string()
     }
+
+    fn key(&self) -> solana_sdk::pubkey::Pubkey {
+        unimplemented!()
+    }
+
+    fn get_reserve_mints(&self) -> Vec<solana_sdk::pubkey::Pubkey> {
+        unimplemented!()
+    }
+
+    fn get_accounts_to_update(&self) -> Vec<solana_sdk::pubkey::Pubkey> {
+        unimplemented!()
+    }
+
+    fn update(&mut self, account_map: &super::AccountMap) -> eyre::Result<()> {
+        unimplemented!()
+    }
+
+    fn quote(&self, quote_params: &crate::adapters::QuoteParams) -> eyre::Result<crate::adapters::Quote> {
+        unimplemented!()
+    }
+
+    fn get_swap_and_account_metas(&self, swap_params: &crate::adapters::SwapParams) -> eyre::Result<crate::adapters::SwapAndAccountMetas> {
+        unimplemented!()
+    }
+
+    fn from_keyed_account(keyed_account: &super::KeyedAccount, amm_context: &super::AmmContext) -> eyre::Result<Self>
+    where
+        Self: Sized,
+    {
+        unimplemented!()
+    }
+
+    fn clone_amm(&self) -> Box<dyn Amm> {
+        unimplemented!()
+    }
 }
